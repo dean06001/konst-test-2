c0328a74df59e64067c1e85b938ef440be0de39e
dean06001 <dean06001@utb.vaxjo.se>
2024-04-03 11:06:42 +0200

    Add project files.

---
+++ konst test 2/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konst_test_2
+{
+    internal class Person
+    {
+        private string namn;
+        private string efternamn;
+
+        public Person(string namnr, string efternamnr)
+        {
+            namn = namnr;
+            efternamn = efternamnr;
+        }
+        public Person()
+        {
+
+        }
+        public string Namn
+        {
+            get { return namn; }
+            set { namn = value; }
+        }
+        public string Efternamn
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+    }
+
+}
+
---
+++ konst test 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace konst_test_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person p = new Person();
+            Console.Write("Skriv namn: ");
+            p.Namn = Console.ReadLine();
+            Console.Write("Skriv efternamn: ");
+            p.Efternamn = Console.ReadLine();
+            Console.WriteLine(p.Namn);
+            Console.WriteLine(p.Efternamn);
+            Console.ReadLine();
+        }
+    }
+}
